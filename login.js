767b462c36f9274c966078e1516e613518f28a4a
Nouval <81620931+nouvalbachrezi@users.noreply.github.com>
2023-12-18 07:39:09 +0700

    Add files via upload

--- login.js
+++ login.js
@@ -1,45 +1,18 @@
 $(document).ready(function() {
 
-    $("#button-registrasi").click(function() {
-        window.location.href = "./registrasi.php"
-    });
-
     $("#button-masuk").click(function() {
-        $("#form-login").submit();        
+        $("#form-login-admin").submit();        
     });
 
-    $("#form-login").validate({
-        rules: {
-            nomor_induk_kependudukan: {
-                minlength: 16,
-                maxlength: 16,
-                numeric: true
-            },
-            password: {
-                minlength: 8,
-                maxlength: 16
-            }
-        },
+    $("#form-login-admin").validate({
         messages: {
-            
-            nomor_induk_kependudukan: {
-                minlength: "Panjang NIK adalah 16 angka",
-                maxlength: "Panjang NIK adalah 16 angka",
-                numeric: "NIK hanya terdiri dari angka",
+            username: {
                 required: "Kolom harus diisi"
             },
             password: {
-                minlength: "Panjang sandi minimal 8 huruf",
-                maxlength: "Panjang sandi maksimal 16 huruf",
                 required: "Kolom harus diisi"
             },
         }
     });
 
 });
-
-$.validator.addMethod("numeric", function(value, element) {
-    return value.match(/^\d+$/);
-}, 
-"Hanya terdiri dari angka"
-);
--- login.php
+++ login.php
@@ -9,13 +9,13 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Login</title>
+        <title>Login Admin</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="icon" type="image/x-icon" href="./images/favicon.ico">
-        <link rel="stylesheet" type="text/css" href="./bootstrap/css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="./css/login_registrasi.css">
-		<link rel="stylesheet" type="text/css" href="./css/font.css">
-		<link rel="stylesheet" type="text/css" href="./css/color.css">
+        <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
+        <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="../css/login_registrasi.css">
+		<link rel="stylesheet" type="text/css" href="../css/font.css">
+		<link rel="stylesheet" type="text/css" href="../css/color.css">
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
         <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.min.js"></script>
     </head>
@@ -24,27 +24,26 @@
             <div class="row p-5 align-middle justify-content-center">
                 <div class="col-6 p-5 rounded-corners bg-light">
                     <div class="col-12">
-                        <form id="form-login" method="post" action="../server/login.php">
+                        <form id="form-login-admin" method="post" action="../../server/login_admin.php">
                             <div class="col-12 d-flex justify-content-center">
                                 <div class="form-outline col-4">
-                                    <img class="img-fluid" src="./images/logo.png" alt="Logo Lemari"/>
+                                    <img class="img-fluid" src="../images/logo.png" alt="Logo Lemari"/>
                                 </div>
                             </div>
-                            <p class="primary-font fs-4 text-center mt-4">Masuk Pendaftaran CPNS</p>
+                            <p class="primary-font fs-4 text-center mt-4">Masuk Admin</p>
     
                             <!-- Input nomor induk kependudukan -->
                             <div class="form-outline mt-4">
-                                <input type="text" name="nomor_induk_kependudukan" id="input_nik" class="form-control rounded-pill border border-success primary-font fs-6 required" value="" placeholder="NIK" />
+                                <input type="text" name="username" id="input-nik" class="form-control rounded-pill border border-success primary-font fs-6 required" value="" placeholder="Username" />
                             </div>
     
                             <!-- Input password -->
                             <div class="form-outline mt-4">
-                                <input type="password" name="password" id="input_password" class="form-control rounded-pill border border-success primary-font fs-6 required" value="" placeholder="Password"/>
+                                <input type="password" name="password" id="input-password" class="form-control rounded-pill border border-success primary-font fs-6 required" value="" placeholder="Password"/>
                             </div>
                         
                             <!-- Tombol registrasi -->
-                            <div class="col d-flex justify-content-between mt-3">
-                                <button id="button-registrasi" type="button" class="btn btn-primary btn-block rounded-pill mb-4 primary-font fs-6">Registrasi</button>
+                            <div class="col d-flex justify-content-end mt-3">
                                 <button id="button-masuk" type="button" class="btn btn-primary btn-block rounded-pill mb-4 primary-font fs-6">Masuk</button>
                             </div>
                         </form>
