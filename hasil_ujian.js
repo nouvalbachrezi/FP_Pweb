00a989416cb5f128ebd1350ec7ed8f068e5796cb
Nouval <81620931+nouvalbachrezi@users.noreply.github.com>
2023-12-18 08:26:34 +0700

    FP PWEB
    PWEB B

--- hasil_ujian.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $("#isi-data-pendaftaran").click(function() {
-        window.location.href = "./isi_data_pendaftaran.php"
-    });
-
-    $("#upload-berkas").click(function() {
-        window.location.href = "./upload-berkas.php";
-    });
-});
--- kartu_ujian.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $("#button-unduh").click(function() {
-        window.location.href = "../server/kartu_ujian.php";
-    });
-});
